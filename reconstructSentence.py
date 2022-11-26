7f904e9fedfcf5476fd06dfcb3837b503b45d620
Joshua Ryan <joshua.ryan@wne.edu>
2022-11-26 19:32:49 +0000

    Uploading Deliverables

--- reconstructSentence.py
+++ reconstructSentence.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+# reconstructSentence.py
+# by Joshua Ryan
+# HW 11 problem 4
+# To run this program use: reconstructSentence.py input1.txt input2.txt
+# This program takes two line arguments: inputFile1 and inputFile2
+
 import sys
 
 input1 = open(sys.argv[1])
